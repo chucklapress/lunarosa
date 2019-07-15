207411604526fe3f234af6cbc91361ee6d4d2848
chucklapress <charleslapress@gmail.com>
2019-07-14 22:31:35 -0400

    added so answer for scraping site

---
+++ so_answer.py
@@ -0,0 +1,26 @@
+from lxml import html
+from time import sleep
+from selenium import webdriver
+
+
+def parse(url):
+    response = webdriver.Chrome()
+    response.get(url)
+    sleep(3)
+    name = response.find_element_by_xpath(' //*[@id="grpDescrip_75-606-157"]')
+    price = response.find_element_by_xpath(' //*[@id="landingpage-price"]/div/div/ul/li[3]')
+    details = response.find_element_by_xpath(' //*[@id="synopsis"]/div[4]/div/div[9]/ul')
+
+
+    print(name.text)
+    print(price.text)
+    print(details.text)
+
+
+
+    sleep(1)
+
+
+if __name__ == '__main__':
+
+    parse('https://www.newegg.ca/p/N82E16875606157')
