5f327e036cfb1c4a49d9dbfdc622bb2e303c2a3e
chucklapress <charleslapress@gmail.com>
2020-09-28 22:33:42 -0400

    re-wrote python flask app and template

--- flask_app/render_flask.py
+++ flask_app/render_flask.py
@@ -2,17 +2,21 @@ from flask import Flask,render_template,request
 import os,glob
 import datetime
 
-
-f=open('/Users/chucklapress/tiy-projects/lunarosa/flavors.txt','r')
-g= f.read()
-
-
 app= Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.datetime.now()
     date = now.strftime("%m-%d-%Y")
-    return render_template('index.html',n=g, **locals())
+    a_file = open('/Users/chucklapress/tiy-projects/lunarosa/flavors.txt')
+    file_contents = a_file.read()
+    sample_list = file_contents.splitlines(True)
+    converted_list = []
+
+    for element in sample_list:
+        converted_list.append(element.strip())
+    for flavor in converted_list:
+        print(flavor)
+    return render_template('index.html', **locals())
 
 
 if __name__=="__main__":
